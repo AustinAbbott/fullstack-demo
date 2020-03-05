8f9ee76004071344a8655318c9eb5bef60bed87a
Austin Abbott <magicaustin51547@gmail.com>
2020-03-04 19:31:07 -0700

    Allow retrieving all bug reports from the server, and a path for bug reports to be added

--- server/db/queries.js
+++ server/db/queries.js
@@ -5,11 +5,14 @@ const pool = new Pool({
     "database": "bugs"
 });
 
-
-function returnAll() {
-    return pool.query("SELECT * FROM bug_reports", (err, res) => {
-        if (err) {
-            throw err
-        }
-    })
+let returnAll = () => {
+    return pool.query("SELECT * FROM bug_reports")
+        .then(results => results.rows)
 }
+
+let addBugReport = (data) => {
+    let values = [data.bug_description, data.reported_by, data.created_date, data.assigned_to, data.threat_level];
+    return pool.query("INSERT INTO bug_reports (bug_description, reported_by, created_date, assigned_to, threat_level) VALUES ($1, $2, $3, $4, $5)", values)
+}
+
+module.exports = { returnAll, addBugReport }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const port = 8080;
+const bodyParser = require('body-parser');
+const db = require('./db/queries.js');
 
-app.get();
+app.use(bodyParser.json());
 
-const port = 3000;
+app.get('/', function(req, res) {
+    db.returnAll()
+    .then(results => res.send(results))
+});
+
+app.post('/', function(req, res) {
+    db.addBugReport(req.body)
+    .then(res.send("It worked!"))
+})
+
 app.listen(port, () => 
 console.log(`Listening on port ${port}`),
 );
